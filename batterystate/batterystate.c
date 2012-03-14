a1423a09d81426a2c176f9b51facb39a691dfb84
Chen Ganir <chen.ganir@ti.com>
2012-03-14 16:01:18 +0200

    Battery: Discover Characteristics
    Add logic to discover all characteristics and build a
    characteristic list

--- batterystate/batterystate.c
+++ batterystate/batterystate.c
@@ -34,14 +34,29 @@
 #include "att.h"
 #include "gatt.h"
 #include "batterystate.h"
+#include "log.h"
 
 struct batterystate {
 	struct btd_device	*dev;		/* Device reference */
 	GAttrib			*attrib;		/* GATT connection */
 	guint			attioid;		/* Att watcher id */
+	struct att_range	*svc_range;	/* DeviceInfo range */
+	GSList			*chars;		/* Characteristics */
 };
 
 static GSList *batteryservices = NULL;
+
+struct characteristic {
+	struct att_char			attr;	/* Characteristic */
+	struct batterystate		*bs;		/* deviceinfo where the char belongs */
+};
+
+static void char_free(gpointer user_data)
+{
+	struct characteristic *c = user_data;
+
+	g_free(c);
+}
 
 static gint cmp_device(gconstpointer a, gconstpointer b)
 {
@@ -64,8 +79,52 @@ static void batterystate_free(gpointer user_data)
 	if (bs->attrib != NULL)
 		g_attrib_unref(bs->attrib);
 
+	if (bs->chars != NULL)
+		g_slist_free_full(bs->chars, char_free);
+
 	btd_device_unref(bs->dev);
+	g_free(bs->svc_range);
 	g_free(bs);
+}
+
+static void configure_batterystate_cb(GSList *characteristics, guint8 status,
+							gpointer user_data)
+{
+	struct batterystate *bs = user_data;
+	GSList *l;
+
+	if (status != 0) {
+		error("Discover batterystate characteristics: %s",
+							att_ecode2str(status));
+		return;
+	}
+
+	for (l = characteristics; l; l = l->next) {
+		struct att_char *c = l->data;
+		struct characteristic *ch;
+		uint16_t start, end;
+
+		ch = g_new0(struct characteristic, 1);
+		ch->attr.handle = c->handle;
+		ch->attr.properties = c->properties;
+		ch->attr.value_handle = c->value_handle;
+		memcpy(ch->attr.uuid, c->uuid, MAX_LEN_UUID_STR + 1);
+		ch->bs = bs;
+
+		bs->chars = g_slist_append(bs->chars, ch);
+
+		start = c->value_handle + 1;
+
+		if (l->next != NULL) {
+			struct att_char *c = l->next->data;
+			if (start == c->handle)
+				continue;
+			end = c->handle - 1;
+		} else if (c->value_handle != bs->svc_range->end)
+			end = bs->svc_range->end;
+		else
+			continue;
+	}
 }
 
 static void attio_connected_cb(GAttrib *attrib, gpointer user_data)
@@ -73,6 +132,10 @@ static void attio_connected_cb(GAttrib *attrib, gpointer user_data)
 	struct batterystate *bs = user_data;
 
 	bs->attrib = g_attrib_ref(attrib);
+
+	gatt_discover_char(bs->attrib, bs->svc_range->start, bs->svc_range->end,
+					NULL, configure_batterystate_cb, bs);
+
 }
 
 static void attio_disconnected_cb(gpointer user_data)
@@ -89,6 +152,9 @@ int batterystate_register(struct btd_device *device, struct att_primary *dattr)
 
 	bs = g_new0(struct batterystate, 1);
 	bs->dev = btd_device_ref(device);
+	bs->svc_range = g_new0(struct att_range, 1);
+	bs->svc_range->start = dattr->start;
+	bs->svc_range->end = dattr->end;
 
 	batteryservices = g_slist_prepend(batteryservices, bs);
 
